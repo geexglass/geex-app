b20da4ec57609c59efa71746311df8c1ab74c089
James Ding <jamesding365@gmail.com>
2025-05-14 16:32:24 -0700

    Update login form branding and adjust layout

--- nextjs-app/components/auth/login-form.tsx
+++ nextjs-app/components/auth/login-form.tsx
@@ -17,7 +17,7 @@ export function LoginForm({
                             <div className="flex flex-col items-center text-center">
                                 <h1 className="text-2xl font-bold">Welcome back</h1>
                                 <p className="text-balance text-muted-foreground">
-                                    Login to your Acme Inc account
+                                    Login to GEEX
                                 </p>
                             </div>
                             <div className="grid gap-2">
@@ -45,9 +45,9 @@ export function LoginForm({
                                 Login
                             </Button>
                             <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
+                            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                              Or continue with
+                            </span>
                             </div>
                             <div className="grid grid-cols-3 gap-4">
                                 <Button variant="outline" className="w-full">
@@ -86,13 +86,13 @@ export function LoginForm({
                             </div>
                         </div>
                     </form>
-                    {/*<div className="relative hidden bg-muted md:block">*/}
-                    {/*    <img*/}
-                    {/*        src="/placeholder.svg"*/}
-                    {/*        alt="Image"*/}
-                    {/*        className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"*/}
-                    {/*    />*/}
-                    {/*</div>*/}
+                    <div className="relative hidden bg-muted md:block">
+                        <img
+                            src="/placeholder.svg"
+                            alt="Image"
+                            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                        />
+                    </div>
                 </CardContent>
             </Card>
             <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
