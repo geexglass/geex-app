6fe0f487f210e609be169a1f766a4449dc1e2b79
James Ding <jamesding365@gmail.com>
2025-05-13 17:26:16 -0700

    User org creation policy disabled

--- nextjs-app/app/lib/auth.ts
+++ nextjs-app/app/lib/auth.ts
@@ -22,6 +22,8 @@ export const auth = betterAuth({
     }),
     plugins: [
         admin(),
-        organization()
+        organization({
+            allowUserToCreateOrganization: false,
+        })
     ]
 })
