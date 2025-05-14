2a269e11b4ec0cfec17f53d9eff430f412093607
James Ding <jamesding365@gmail.com>
2025-05-13 20:44:39 -0700

    Fix import path for seedAdmin in layout component

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -15,7 +15,7 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
 import { handleError } from "./client-utils";
-import { seedAdmin } from "./lib/seed-admin";
+import { seedAdmin } from "@/lib/seed-admin";
 
 /**
  * Generate metadata for the page.
